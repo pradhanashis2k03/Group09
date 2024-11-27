f80a818ce2bea9e1c8d57eae933efef0416d7f28
pradhanashis2k03 <100114532+pradhanashis2k03@users.noreply.github.com>
2024-11-27 23:27:27 +0530

    Update main.c
    fixed the delay function

--- main.c
+++ main.c
@@ -348,7 +348,11 @@ void Delay_ms(int time_ms)
 {
     int i, j;
     for(i = 0 ; i < time_ms; i++)
-        for(j = 0; j < 3180; j++)
+        for(j = 0; j < 3180; j++){
+          j++;
+        }
+  i++;
+  }
 }
 void SystemInit(void)
 {
